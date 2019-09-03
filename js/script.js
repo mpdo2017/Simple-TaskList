1f6bb6c971bcb50ad50b45dc51c897a8571da0ff
Wes Lanning <lanning.52@gmail.com>
2019-09-03 17:44:52 -0400

    Cleaned up some of the javascript code.

--- js/script.js
+++ js/script.js
@@ -1,37 +1,51 @@
-document.body.onload = createNewItem;
+"use strict";
+let addTaskBtn = document.getElementById("addTask");
 
-let form = document.getElementById("toDoList");
-let name = document.getElementsByClassName("name");
-let description = document.getElementsByClassName("description");
-let date = document.getElementsByClassName("date");
+window.addEventListener('load', () => {
+    console.log('All assets are loaded');
+    addTaskBtn.addEventListener('click', createNewItem);
+});
+
+
 
 function createNewItem() {
+    let newTaskName = document.getElementById("name");
+    let newTaskDescription = document.getElementById("description");
+    let newTaskDate = document.getElementById("date");
 
-    var listForm = document.forms["toDoList"];
-    var ul = document.getElementsByClassName("form");
-    var makeTask = document.createElement("li");
-    var addToList = document.createTextNode("_" + newTask);
-    makeTask.appendChild(addToList);
-    var ul  = document.getElementById("list2");
-    document.body.insertBefore(makeTask, ul);
+    let taskList = document.getElementById("taskList");
+    let newTaskItem = document.createElement("li");
+    let newTaskSpan = document.createElement("span");
+    let removeTaskBtn = document.createElement("button");
+    removeTaskBtn.addEventListener("click", remove);
 
-    var removefromList = .removeChild()
-
+    // create the text node to be added to the list item
+    let newTaskText = document.createTextNode(`${newTaskName.value} | ${newTaskDescription.value} | ${newTaskDate.value}`);
+    // add text to the list item
+    newTaskSpan.appendChild(newTaskText);
+    newTaskItem.appendChild(newTaskSpan);
+    newTaskItem.appendChild(removeTaskBtn);
+    // append the new list item with text to the unordered list
+    taskList.appendChild(newTaskItem);
 }
-function submit() {
 
+function submit() {
     document.getElementById("toDoList").addEventListener("click", createNewItem);
     form.onclick = submit;
-
-
 }
-function remove(){
 
-    document.getElementById("toDoList").addEventListener("click", deleteTaskItem);
-    form.onclick = remove;
-
-
+function remove(event) {
+    let li = event.target.parentElement;
+    //let ul = li.parentElement;
+    //const index = Array.from(e.parentElement.parentElement.children).indexOf(e);
+    li.remove();
+    //console.log(ul);
+    //const index = Array.from(ul.children).indexOf(li);
+    //console.log(`index: ${index}`);
+    //document.getElementById("toDoList").addEventListener("click", deleteTaskItem);
+    // form.onclick = remove;
 }
+
 function preventDefault() {
     Event.preventDefault();
 
